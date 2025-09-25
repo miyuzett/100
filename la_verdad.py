56fba3becd23a4d1a0f4a4939d86ab6dc1597914
miyuzett <monserratt.nincolaf@estudiantes.colegiotgs.cl>
2025-09-25 15:10:00 -0300

    Update la_verdad.py

--- la_verdad.py
+++ la_verdad.py
@@ -7,20 +7,21 @@ lol , lal = st.columns(2)
 #entrevista a emilia muñoz y 2 imagenes de una chica y un entrevista
 lol.write("**Entrevistador**:Emili- DIGO Azharx, dinos la verdad realmente el niño sam te pago para que dijeras que lo viste morir???????????????")
 lol.write("Azharx: en efecto!, eso que habeis dicho esta en lo correcto, el malicioso niño sam me a pagado para que no os dijera la verdad!, el realmente es un malujo que roba dinerillo de sus fans")
-lol.image("C:/Users/user_/OneDrive/Documentos/niño sam/pngtree-frightened-woman-in-hallway-worry-lady-shadow-png-image_14251851.png")
+lol.image("pngtree-frightened-woman-in-hallway-worry-lady-shadow-png-image_14251851.png")
 #audio de emilia muñoz hablando y mas lore
-lol.audio("C:/Users/user_/OneDrive/Documentos/niño sam/audiocleaner_20250922_155622_translator.mp3")
+lol.audio("audiocleaner_20250922_155622_translator.mp3")
 lol.write("**estrevistador**:porque aceptaste el dinero, a pesar de que sabias de lo que podria pasar con esa noticia?")
 
 #emilia hablando y 2 imagenes de lo mismo de arriba
 lol.write("Azharx: acepte el dinerillo sin pensar ya que si no lo aceptaba me ubieran asesinado.., asi es, el niño sam es un tipajo muy malete y sinverguenza")
-lol.image("C:/Users/user_/OneDrive/Documentos/niño sam/pngtree-scared-woman-defending-herself-funny-png-image_9959841.png")
-lal.image("C:/Users/user_/OneDrive/Documentos/niño sam/hombre-negocios-enojado-gafas-moda-haciendo-muecas_329181-601.png")
+lol.image("pngtree-scared-woman-defending-herself-funny-png-image_9959841.png")
+lal.image("hombre-negocios-enojado-gafas-moda-haciendo-muecas_329181-601.png")
 
 #mas de la entrevista y 1 imagen de un hombre de negocios 
 lal.write("**entrevistador**:por cuanto tiempo ocultaste la verdad?")
 lal.write("Azharx: he ocultado la verdad bajo el tapete por mas de 4 años:unlock:, me involucre tanto en su polemica que ahora estoy aqui siendo la primera que os cuenta la verdad")
-lal.image("C:/Users/user_/OneDrive/Documentos/niño sam/hombre-negocios-enojado-gafas-moda-haciendo-muecas_329181-601.png")
+lal.image("hombre-negocios-enojado-gafas-moda-haciendo-muecas_329181-601.png")
+
 
 
 
